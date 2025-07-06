92b0cdef0529754536045a494182b5331db66898
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2025-07-06 12:05:30 +0200

    Add tailwindcss to the project

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "wxt";
+import { defineConfig, WxtViteConfig } from "wxt";
+import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  vite: () => ({
+    plugins: [tailwindcss()],
+  } as WxtViteConfig),
   modules: ["@wxt-dev/module-react", "@wxt-dev/i18n/module"],
   manifest: {
     default_locale: "en",
