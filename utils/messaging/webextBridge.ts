67f9609a46ef19e8b00e468e045f52ed7146a1b1
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2025-07-23 00:43:44 +0200

    Webext bridge protocol for getting image prediction cache

--- utils/messaging/webextBridge.ts
+++ utils/messaging/webextBridge.ts
@@ -1,5 +1,7 @@
-import type { IHostSettings } from '../db/hostSettings';
 import type { ProtocolWithReturn } from 'webext-bridge';
+
+import type { IHostSettings } from '../db/hostSettings';
+import { IImagePrediction } from '../db/predictionCache';
 
 /**
  * Protocol for the bridge to communicate between different parts of the extension
@@ -14,5 +16,7 @@ declare module 'webext-bridge' {
 
     // Notify content scripts of settings changes
     HOST_SETTINGS_UPDATED: ProtocolWithReturn<{ hostname: string }, void>;
+
+    GET_HOSTNAME_IMAGE_PREDICTION_CACHE: ProtocolWithReturn<{ hostname: string }, IImagePrediction[]>;
   }
 }
