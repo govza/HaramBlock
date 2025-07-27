ae84068495abafce74d4ab9fbfead1d89d7b07e5
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2025-07-27 21:21:15 +0200

    Fix utf8 issues on production build

---
+++ scripts/vite-plugin-to-utf8.ts
@@ -0,0 +1,29 @@
+import { PluginOption } from "vite";
+
+function strToUtf8(str: string) {
+  return str
+    .split("")
+    .map((ch) =>
+      ch.charCodeAt(0) <= 0x7f
+        ? ch
+        : "\\u" + ("0000" + ch.charCodeAt(0).toString(16)).slice(-4)
+    )
+    .join("");
+}
+
+export default function toUtf8(): PluginOption {
+  return {
+    name: "to-utf8",
+    generateBundle(options, bundle) {
+      for (const fileName in bundle) {
+        const chunk = bundle[fileName];
+        if (chunk && chunk.type === "chunk" && typeof chunk.code === "string") {
+          const originalCode = chunk.code;
+          const modifiedCode = strToUtf8(originalCode);
+
+          chunk.code = modifiedCode;
+        }
+      }
+    },
+  };
+}
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig, WxtViteConfig } from "wxt";
 import tailwindcss from "@tailwindcss/vite";
+import toUtf8 from "./scripts/vite-plugin-to-utf8";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   vite: () => ({
-    plugins: [tailwindcss()],
+    plugins: [toUtf8(), tailwindcss()],
   } as WxtViteConfig),
   modules: ["@wxt-dev/module-react", "@wxt-dev/i18n/module"],
   manifest: {
