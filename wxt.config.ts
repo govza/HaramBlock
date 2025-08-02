fbb429d3e7ec7a1e3536e4b758536e1f5f140260
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2025-08-03 01:43:37 +0200

    Prettier with eslint

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig, WxtViteConfig } from "wxt";
-import tailwindcss from "@tailwindcss/vite";
-import toUtf8 from "./scripts/vite-plugin-to-utf8";
+import { defineConfig, type WxtViteConfig } from 'wxt';
+import tailwindcss from '@tailwindcss/vite';
+import toUtf8 from './scripts/vite-plugin-to-utf8';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  vite: () => ({
-    plugins: [toUtf8(), tailwindcss()],
-  } as WxtViteConfig),
-  modules: ["@wxt-dev/module-react", "@wxt-dev/i18n/module"],
+  vite: () =>
+    ({
+      plugins: [toUtf8(), tailwindcss()],
+    }) as WxtViteConfig,
+  modules: ['@wxt-dev/module-react', '@wxt-dev/i18n/module'],
   manifest: {
-    default_locale: "en",
+    default_locale: 'en',
     permissions: ['storage', 'tabs', 'activeTab', 'scripting'],
   },
 });
