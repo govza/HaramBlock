fc95f36fc28eb0d0f3663178bf78583c24ca8029
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2025-08-03 23:35:22 +0200

    Strictness filter

--- entrypoints/popup/components/Content.tsx
+++ entrypoints/popup/components/Content.tsx
@@ -1,9 +1,11 @@
 import { Outline } from '@/entrypoints/popup/components/Outline';
+import { Strictness } from '@/entrypoints/popup/components/Strictness';
 
 export const Content = () => {
   return (
     <div className='grow px-2'>
       <Outline />
+      <Strictness />
     </div>
   );
 };
---
+++ entrypoints/popup/components/Strictness.tsx
@@ -0,0 +1,37 @@
+import { useHostDataContext } from '@/entrypoints/popup/context/HostDataContext';
+import { t } from '@/utils/i18n';
+
+export const Strictness = () => {
+  const { hostSettings } = useHostDataContext();
+
+  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(event.target.value);
+    await hostSettings.setStrictness(value);
+
+    if (value === 0) {
+      await hostSettings.setPolicy('whitelist');
+    } else {
+      await hostSettings.setPolicy('process');
+    }
+  };
+
+  return (
+    <div className='my-2 flex flex-col text-sm'>
+      <label htmlFor='strictness-slider '>
+        {t('strictness')} {`${(hostSettings.strictness * 100).toFixed()} %`}
+      </label>
+      <input
+        id='strictness-slider'
+        type='range'
+        min='0'
+        max='1'
+        step='0.01'
+        value={hostSettings.strictness}
+        onChange={e => {
+          void handleChange(e);
+        }}
+        className='w-full accent-blue-500 md:w-1/2'
+      />
+    </div>
+  );
+};
