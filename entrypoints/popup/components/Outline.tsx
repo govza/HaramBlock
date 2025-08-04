8f23d3b9da821e1c647c1ad87d5e2aaba67a1f6b
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2025-08-05 00:26:31 +0200

    Update Outline component to use hostSettingsRepository for state management and add 'full' option to OutlineType

--- entrypoints/popup/components/Outline.tsx
+++ entrypoints/popup/components/Outline.tsx
@@ -1,16 +1,22 @@
 import { useHostDataContext } from '@/entrypoints/popup/context/HostDataContext';
 import { t } from '@/utils/i18n';
 
+import type { OutlineType } from '@/utils/types';
+
 export const Outline = () => {
-  const { hostSettings } = useHostDataContext();
+  const { hostSettings, hostSettingsRepository } = useHostDataContext();
 
   const handleChange = (outline: OutlineType) => {
-    void hostSettings.setOutline(outline);
+    void hostSettingsRepository.setOutline(hostSettings.hostname, outline);
   };
 
   return (
-    <div className='my-2 flex flex-col rounded-full bg-gray-400 text-sm'>
-      <div className='flex rounded-full bg-gray-400 p-1'>
+    <div className='my-2 flex items-center gap-2 text-sm'>
+      <div className={`flex flex-1 rounded-full bg-gray-400 p-1 transition ${
+        hostSettings.outline === 'full' 
+          ? 'opacity-50' 
+          : ''
+      }`}>
         <button
           className={`flex-1 rounded-full p-1 text-center transition cursor-pointer ${
             hostSettings.outline === 'bbox' ? 'bg-gray-600' : 'text-gray-800'
@@ -22,12 +28,40 @@ export const Outline = () => {
         <button
           className={`flex-1 rounded-full p-1 text-center transition cursor-pointer ${
             hostSettings.outline === 'segment'
-              ? 'bg-gray-600 text-gray-100'
+              ? 'bg-gray-600'
               : 'text-gray-800'
           }`}
           onClick={() => handleChange('segment')}
         >
           {t('outlineSegment')}
+        </button>
+      </div>
+      <div className={`w-9 h-9 rounded-full transition p-1 ${
+        hostSettings.outline === 'full'
+          ? 'bg-gray-400 ring-2 ring-inset ring-gray-400'
+          : 'bg-gray-400'
+      }`}>
+        <button
+          className={`w-full h-full rounded-full cursor-pointer flex items-center justify-center transition ${
+            hostSettings.outline === 'full'
+              ? 'bg-gray-600 text-red-900'
+              : 'text-gray-700'
+          }`}
+          onClick={() => handleChange('full')}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-6'
+          >
+            <path
+              strokeLinecap='round'
+              d='M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+            />
+          </svg>
         </button>
       </div>
     </div>
