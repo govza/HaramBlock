49ebd4a50241ee513642367b4efa90a1abd6b48f
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2025-08-17 23:40:06 +0200

    vitest config linter error fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import { WxtVitest } from 'wxt/testing';
 
+import type { PluginOption } from 'vite';
+
 export default defineConfig({
   // Configure test behavior however you like
   test: {
@@ -9,8 +11,7 @@ export default defineConfig({
     watch: false,
   },
 
-  // @ts-expect-error - WxtVitest plugin types may not be fully compatible
-  plugins: [WxtVitest()],
+  plugins: [WxtVitest() as unknown as PluginOption],
 
   // If any dependencies rely on webextension-polyfill, add them here to the `ssr.noExternal` option.
   // Example:
