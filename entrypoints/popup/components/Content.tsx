aab04ee1aa7ec9b274dbdff696e06e9ebf5b9443
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2026-01-02 18:12:00 +0100

    feat: implement FlipCard component for enhanced settings navigation and update UI layout

--- entrypoints/popup/components/Content.tsx
+++ entrypoints/popup/components/Content.tsx
@@ -2,12 +2,14 @@ import { BlurIntensity } from '@/entrypoints/popup/components/BlurIntensity';
 import { BlurTint } from '@/entrypoints/popup/components/BlurTint';
 import { Outline } from '@/entrypoints/popup/components/Outline';
 import { PixelationScale } from '@/entrypoints/popup/components/PixelationScale';
+import { PolicyButton } from '@/entrypoints/popup/components/PolicyButton';
 import { QuickToggleSetting } from '@/entrypoints/popup/components/QuickToggleSetting';
 import { Strictness } from '@/entrypoints/popup/components/Strictness';
 
 export const Content = () => {
   return (
-    <div className='grow px-2'>
+    <div className='px-2 pt-2'>
+      <PolicyButton />
       <Outline />
       <Strictness />
       <BlurTint />
