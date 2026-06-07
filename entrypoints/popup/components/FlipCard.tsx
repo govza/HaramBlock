3d19847f4ac330f7a1602c8582d41389b3e83265
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2026-06-07 12:43:26 +0200

    Fix flip card mirroring issues

--- entrypoints/popup/components/FlipCard.tsx
+++ entrypoints/popup/components/FlipCard.tsx
@@ -26,8 +26,11 @@ type FlipCardProps = {
 export const FlipCard = ({ children, isFlipped, onFlip }: FlipCardProps) => {
   return (
     <FlipCardContext.Provider value={{ isFlipped, flip: onFlip }}>
-      <div className='w-full overflow-hidden perspective-[1000px]'>
-        <div className={`grid transition-transform duration-500 transform-3d ${isFlipped ? 'rotate-y-180' : ''}`}>
+      <div className='w-full overflow-hidden' style={{ perspective: '1000px' }}>
+        <div
+          className={`grid transition-transform duration-500 ${isFlipped ? 'rotate-y-180' : ''}`}
+          style={{ transformStyle: 'preserve-3d' }}
+        >
           {children}
         </div>
       </div>
@@ -39,13 +42,19 @@ type FlipCardSideProps = {
   children: ReactNode;
 };
 
+const backfaceStyle = { backfaceVisibility: 'hidden' as const };
+
 const Front = ({ children }: FlipCardSideProps) => {
-  return <div className='col-start-1 row-start-1 rotate-y-0 backface-hidden'>{children}</div>;
+  return (
+    <div className='col-start-1 row-start-1 rotate-y-0' style={backfaceStyle}>
+      {children}
+    </div>
+  );
 };
 
 const Back = ({ children }: FlipCardSideProps) => {
   return (
-    <div className='relative col-start-1 row-start-1 backface-hidden rotate-y-180'>
+    <div className='relative col-start-1 row-start-1 rotate-y-180' style={backfaceStyle}>
       {children}
       <div className='pointer-events-none absolute inset-0 bg-danger/10' />
     </div>
